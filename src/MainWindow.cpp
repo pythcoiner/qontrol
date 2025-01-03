1d1490b9b158dfbbec1242a40ef704baaac589dd
pythcoiner <pythcoiner@proton.me>
2025-01-03 17:55:56 +0100

    MainWindow: cleanup

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -4,8 +4,6 @@
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
     Controller::init();
     Controller::get()->start(this);
-    setWindowTitle("Uno Wizard");
-    setFixedSize(800, 800);
     this-> m_central_widget = new QWidget(this);
     this->setCentralWidget(m_central_widget);
 }
