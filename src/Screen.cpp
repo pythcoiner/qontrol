c310c9311afa6c1b49cfbe358ec24b3b9a80844d
pythcoiner <pythcoiner@proton.me>
2026-01-20 04:36:27 +0100

    fix(Screen): remove dead widget registry code

--- src/Screen.cpp
+++ src/Screen.cpp
@@ -1,11 +1,5 @@
 #include "Screen.h"
 #include "Controller.h"
-#include "widgets/ComboBox.h"
-#include <memory>
-#include <qcheckbox.h>
-#include <qcombobox.h>
-#include <qlineedit.h>
-#include <qwidget.h>
 
 namespace qontrol {
 
@@ -15,65 +9,6 @@ Screen::Screen() {
 
 auto Screen::className() const -> const char * {
     return this->metaObject()->className();
-}
-
-#define GETTER(member, typeName)                                                                                       \
-    if (!this->member.contains(name)) {                                                                                \
-        qDebug() << this->className() << " does not contain " << typeName << " with name " << name;                    \
-        return nullptr;                                                                                                \
-    }                                                                                                                  \
-    return this->member.value(name);
-
-auto Screen::button(const QString &name) -> QPushButton * {
-    GETTER(m_buttons, "button")
-}
-
-auto Screen::input(const QString &name) -> QLineEdit * {
-    GETTER(m_inputs, "input")
-}
-
-auto Screen::checkbox(const QString &name) -> QCheckBox * {
-    GETTER(m_checkboxes, "checkbox")
-}
-
-auto Screen::label(const QString &name) -> QLabel * {
-    GETTER(m_labels, "label")
-}
-
-auto Screen::slider(const QString &name) -> widgets::Slider * {
-    GETTER(m_sliders, "slider")
-}
-
-auto Screen::combobox(const QString &name) -> widgets::ComboBox *{GETTER(m_comboboxes, "combobox")}
-
-#define INSERT(member)                                                                                                 \
-    widget->setParent(this);                                                                                           \
-    auto *replaced = this->member.take(name);                                                                          \
-    this->member.insert(name, widget);                                                                                 \
-    delete replaced;
-
-void Screen::insert(const QString &name, QPushButton *widget) {
-    INSERT(m_buttons)
-}
-
-void Screen::insert(const QString &name, QLineEdit *widget) {
-    INSERT(m_inputs)
-}
-
-void Screen::insert(const QString &name, QCheckBox *widget) {
-    INSERT(m_checkboxes)
-}
-
-void Screen::insert(const QString &name, QLabel *widget) {
-    INSERT(m_labels)
-}
-
-void Screen::insert(const QString &name, widgets::Slider *widget) {
-    INSERT(m_sliders)
-}
-
-void Screen::insert(const QString &name, widgets::ComboBox *widget) {
-    INSERT(m_comboboxes)
 }
 
 } // namespace qontrol
--- src/Screen.h
+++ src/Screen.h
@@ -1,46 +1,15 @@
 #pragma once
 
-#include "widgets/Slider.h"
-#include <QCheckBox>
-#include <QLabel>
-#include <QLineEdit>
+#include "widgets/Widget.h"
 #include <QWidget>
-#include <qcheckbox.h>
-#include <qhash.h>
-#include <qlineedit.h>
-#include <qlogging.h>
-#include <qobject.h>
-#include <qpushbutton.h>
 
 namespace qontrol {
-
-class Controller;
-
-namespace widgets {
-
-class ComboBox;
-
-} // namespace widgets
 
 class Screen : public QWidget, public qontrol::widgets::Widget {
     Q_OBJECT
 public:
     explicit Screen();
     ~Screen() override = default;
-
-    auto button(const QString &name) -> QPushButton *;
-    auto input(const QString &name) -> QLineEdit *;
-    auto checkbox(const QString &name) -> QCheckBox *;
-    auto label(const QString &name) -> QLabel *;
-    auto slider(const QString &name) -> widgets::Slider *;
-    auto combobox(const QString &name) -> widgets::ComboBox *;
-
-    void insert(const QString &name, QPushButton *widget);
-    void insert(const QString &name, QLineEdit *widget);
-    void insert(const QString &name, QCheckBox *widget);
-    void insert(const QString &name, QLabel *widget);
-    void insert(const QString &name, widgets::Slider *widget);
-    void insert(const QString &name, widgets::ComboBox *widget);
 
 public slots:
     virtual void onUnload() {};
@@ -54,14 +23,6 @@ protected:
     virtual void view() = 0;
     // connection that should be done on screen loading
     virtual void doConnect() = 0;
-
-private:
-    QHash<QString, QPushButton *> m_buttons;
-    QHash<QString, QLineEdit *> m_inputs;
-    QHash<QString, QCheckBox *> m_checkboxes;
-    QHash<QString, QLabel *> m_labels;
-    QHash<QString, widgets::Slider *> m_sliders;
-    QHash<QString, widgets::ComboBox *> m_comboboxes;
 };
 
 } // namespace qontrol
